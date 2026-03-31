c008b3ce1fba8e037dc36479f9fb47e5509f0f62
ryoj <r.okamoton@gmail.com>
2026-03-31 18:26:52 +0900

    update for tmux

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,5 @@
 vim.loader.enable()
+vim.loader.enable()
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.g.mapleader = " "
@@ -39,7 +40,6 @@ opt.showmatch = true
 opt.termguicolors = true
 opt.background = "dark"
 
-opt.clipboard:append("unnamedplus")
 
 local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
@@ -61,6 +61,82 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.wo[0][0].foldmethod = 'expr'
   end,
 })
+
+local function readfile(path)
+  local f = io.open(path, "r")
+  if not f then
+    return nil
+  end
+  local s = f:read("*a")
+  f:close()
+  return s
+end
+
+local function proc_comm_and_ppid(pid)
+  local stat = readfile("/proc/" .. pid .. "/stat")
+  if not stat then
+    return nil, nil
+  end
+
+  -- /proc/<pid>/stat:
+  -- pid (comm) state ppid ...
+  local comm, ppid = stat:match("^%d+ %((.-)%) . (%d+)")
+  return comm, ppid
+end
+
+
+local function gui_clipboard_provider()
+  if vim.env.WAYLAND_DISPLAY and vim.fn.executable("waycopy") == 1 and vim.fn.executable("waypaste") == 1 then
+    return "wayclip"
+  elseif vim.env.WAYLAND_DISPLAY and vim.fn.executable("wl-copy") == 1 and vim.fn.executable("wl-paste") == 1 then
+    return "wl-copy"
+  elseif vim.env.DISPLAY and vim.fn.executable("xclip") == 1 then
+    return "xclip"
+  elseif vim.env.DISPLAY and vim.fn.executable("xsel") == 1 then
+    return "xsel"
+  else
+    return nil
+  end
+end
+
+local function is_tmux_client_over_ssh()
+  if not vim.env.TMUX or vim.env.TMUX == "" then
+    return false
+  end
+  local pid = vim.fn.system("tmux display-message -p '#{client_pid}'"):gsub("%s+", "")
+  if pid == "" then
+    return false
+  end
+
+  while pid ~= "" and pid ~= "1" do
+    local comm, ppid = proc_comm_and_ppid(pid)
+    if not comm then
+      return false
+    end
+    if comm == "sshd" then
+      return true
+    end
+    pid = ppid or ""
+  end
+
+  return false
+end
+
+local function set_clipboard()
+  if is_tmux_client_over_ssh() then
+    vim.g.clipboard = "osc52"
+  else
+    vim.g.clipboard = gui_clipboard_provider() or "osc52"
+  end
+end
+
+vim.api.nvim_create_autocmd({ "FocusGained", "VimResume" }, {
+  callback = set_clipboard,
+})
+
+set_clipboard()
+
+opt.clipboard:append("unnamedplus")
 
 require("lazyvim")
 
